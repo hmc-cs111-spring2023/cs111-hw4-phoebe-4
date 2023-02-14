d5c1a8713e0e95df79c62125583fc5b750a935b9
Phoebe C <63136975+pc027@users.noreply.github.com>
2023-02-14 16:49:06 +0000

    complete collatz function and test

--- src/main/scala/collatz/Collatz.scala
+++ src/main/scala/collatz/Collatz.scala
@@ -1,1 +1,3 @@
-
+@main def collatz(n: Int) =
+    if n % 2 == 0 then n / 2
+    else (3 * n) + 1 
--- src/test/scala/collatz/CollatzSpec.scala
+++ src/test/scala/collatz/CollatzSpec.scala
@@ -4,5 +4,28 @@ import org.scalatest.matchers.should._
 class CollatzSpec extends AnyFunSuite with Matchers:
 
 // write tests here!
+    test("collatz 1") {
+        collatz(1) should be (4)
+    }
+
+    test("collatz 2") {
+        collatz(2) should be (1)
+    }
+
+    test("collatz 6") {
+        collatz(6) should be (3)
+    }
+
+    test("collatz 7") {
+        collatz(7) should be (22)
+    }
+
+    test("collatz 42") {
+        collatz(42) should be (21)
+    }
+
+    test("collatz 101") {
+        collatz(101) should be (304)
+    }
 
 end CollatzSpec
